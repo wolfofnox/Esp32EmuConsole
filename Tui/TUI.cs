c56fb4aa15b64aeefa9d106278fad63cf802efa7
wolfofnox <137057355+wolfofnox@users.noreply.github.com>
2026-01-31 16:09:43 +0100

    very basic app tui v1

--- Tui/TUI.cs
+++ Tui/TUI.cs
@@ -18,18 +18,101 @@ public class TUI
     {
         Application.Init();
 
-        var label = new Label("Press ANY key... (Ctrl+C to force quit)")
+        var top = Application.Top;
+
+        var win = new Window("Esp32EmuConsole TUI")
+        {
+            X = 0,
+            Y = 2,
+
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        top.Add(win);
+
+        var menu = new MenuBar(new MenuBarItem[]
+        {
+            new MenuBarItem("(use Alt)", new MenuItem[0]),
+            new MenuBarItem("_File", new MenuItem[]
+            {
+                new MenuItem("_Quit", "", () => { Application.RequestStop(); })
+            }),
+            new MenuBarItem("_Edit", new MenuItem[]
+            {
+                new MenuItem("_Settings", "", () =>
+                {
+                    MessageBox.Query(50, 7, "Settings", "Settings dialog not implemented yet.", "Ok");
+                }),
+                new MenuItem("Static _Responses", "", () =>
+                {
+                    MessageBox.Query(50, 7, "Static Responses", "Static Responses dialog not implemented yet.", "Ok");
+                })
+            }),
+            new MenuBarItem("_Help", new MenuItem[]
+            {
+                new MenuItem("_About", "", () =>
+                {
+                    MessageBox.Query(50, 7, "About Esp32EmuConsole", "Esp32EmuConsole TUI\nVersion 1.0.0", "Ok");
+                })
+            })
+        })
+        {
+            Y = 1
+        };
+
+        top.Add(menu);
+
+        var topLabel = new Label("Terminal.Gui Application - Press Alt for Menu")
+        {
+            X = 0,
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = 1,
+            ColorScheme = new ColorScheme()
+            {
+                Normal = Application.Driver.MakeAttribute(Color.Black, Color.Gray)
+            }
+        };
+        top.Add(topLabel);
+
+
+        var label = new Label("Press ANY key... (Ctrl+C to quit)")
         {
             X = Pos.Center(),
             Y = Pos.Center()
         };
 
-        Application.Top.Add(label);
+        win.Add(label);
 
-        Application.Top.KeyPress += (e) =>
+        top.KeyPress += (e) =>
         {
-            label.Text = $"Key detected: {e.KeyEvent.Key}";
+            label.Text = $"Key detected on top: {e.KeyEvent.Key}";
             Application.Refresh();
+            if (e.KeyEvent.Key == (Key.CtrlMask | Key.C))
+            {
+                Application.RequestStop();
+            }
+        };
+
+        win.KeyPress += (e) =>
+        {
+            label.Text = $"Key detected on win: {e.KeyEvent.Key}";
+            Application.Refresh();
+            if (e.KeyEvent.Key == (Key.CtrlMask | Key.C))
+            {
+                Application.RequestStop();
+            }
+        };
+
+        menu.KeyPress += (e) =>
+        {
+            label.Text = $"Key detected on menu: {e.KeyEvent.Key}";
+            Application.Refresh();
+            if (e.KeyEvent.Key == (Key.CtrlMask | Key.C))
+            {
+                Application.RequestStop();
+            }
         };
 
         Application.Run();
